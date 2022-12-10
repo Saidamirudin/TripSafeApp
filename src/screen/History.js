e7f3f400bedc38611c8b0582b10a21e270df3742
Nur Said Amirudin <saidammir80@gmail.com>
2022-12-10 11:24:54 +0700

    seven commit

--- src/screen/History.js
+++ src/screen/History.js
@@ -1,14 +1,86 @@
 import React from 'react'
-import { ScrollView, Text } from 'react-native'
+import { ScrollView, Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+
+
+const style = StyleSheet.create({
+    topText: {
+        backgroundColor: '#00AA13',
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    txtTop: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 19
+    },
+    cardWrap: {
+        backgroundColor: '#D8D4D4',
+        padding: 10,
+        margin: 20,
+        borderRadius: 10
+    },
+    dateCard: {
+        fontWeight: '700',
+        fontSize: 16,
+        color: '#ED6C30'
+    },
+    txtWrap: {
+        marginTop: 10
+    },
+    flxText: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    txtHis: {
+        color: 'black',
+        fontSize: 16
+    }, 
+    txtHis2: {
+        fontSize: 16
+    }
+})
+
+const CardHistory = () => {
+    return (
+        <TouchableOpacity style={style.cardWrap}>
+                <Text style={style.dateCard}>18 Oktober 2022</Text>
+
+                <View style={style.txtWrap}>
+                    <View style={style.flxText}>
+                        <Text style={style.txtHis}>Lokasi Kejadian</Text>
+                        <Text style={style.txtHis2}>: Jalan Pahlawan</Text>
+                    </View>
+                    <View style={style.flxText}>
+                        <Text style={style.txtHis}>Kendala</Text>
+                        <Text style={style.txtHis2}>: Kehabisan BBM</Text>
+                    </View>
+                    <View style={style.flxText}>
+                        <Text style={style.txtHis}>Tempat Tujuan</Text>
+                        <Text style={style.txtHis2}>: SPBU Pahlawan</Text>
+                    </View>
+                    <View style={style.flxText}>
+                        <Text style={style.txtHis}>Jam</Text>
+                        <Text style={style.txtHis2}>: Pukul 20.14 WIB</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+    )
+}
 
 const History = () => {
     return (
         <ScrollView>
-            <Text>
-                History Menu
-            </Text>
+            <View style={style.topText}>
+                <Text style={style.txtTop}>
+                    Riwayat
+                </Text>
+            </View>
+            {/* komponen card */}
+            <CardHistory/>
+
         </ScrollView>
     )
 }
 
-export default History
+export default History;
