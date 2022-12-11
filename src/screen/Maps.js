8aed146f40a13b57a4205e95f229ba16612a4106
Nur Said Amirudin <saidammir80@gmail.com>
2022-12-11 22:47:20 +0700

    nine commit

--- src/screen/Maps.js
+++ src/screen/Maps.js
@@ -1,13 +1,49 @@
 import React from 'react'
-import { ScrollView, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+// import MapView from 'react-native-maps'
+
+
+const style = StyleSheet.create({
+    topText: {
+        backgroundColor: '#00AA13',
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    txtTop: {
+        color: 'white',
+        fontSize: 19,
+        fontWeight: 'bold'
+    },
+})
 
 const Maps = () => {
     return (
-        <ScrollView>
-            <Text>
-                Maps Menu
-            </Text>
-        </ScrollView>
+        <View>
+            <View style={style.topText}>
+                <Text style={style.txtTop}>
+                    Maps - Lokasi Anda Sekarang
+                </Text>
+            </View>
+            {/* <MapView
+                style={{ flex: 1 }}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            >
+                <MapView.Marker
+                    coordinate={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                    }}
+                    title="My Marker"
+                    description="Some description"
+                />
+            </MapView> */}
+        </View>
     )
 }
 
